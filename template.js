ab83a672e6e7afffec1f5176afae39b92da24d98
Pavel Bucka <ppenge@gmail.com>
2019-10-15 08:38:47 +0000

    Use toc-to-html
    - update styles for "a" and "ul"

--- template.js
+++ template.js
@@ -1,7 +1,15 @@
 const path = require('path');
 const pug = require('pug');
+const tocToHtml = require('toc-to-html');
 
 const file = path.resolve(__dirname, 'template.pug');
 const fn = pug.compileFile(file);
 
-module.exports = fn;
+module.exports = data => {
+  const toc = tocToHtml(data.toc, {
+    id: 'toc',
+    clazz: 'hidden'
+  });
+
+  return fn({ ...data, toc });
+};
